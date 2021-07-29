1e79f5f3d75b84e23ec4ccd1bfb544f5ab7f6423
shalahuddin-ali <m.shalahuddin.ali@gmail.com>
2021-07-30 06:06:27 +0800

    pull from remote

--- server.js
+++ server.js
@@ -7,16 +7,22 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-	});
-}
 app.use(cors());
 app.use(express.json());
 
 app.use('/proxyServer', proxyRouter);
 app.use('/admin', adminRouter);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.json({ message: 'API Connected...' });
+	});
+}
 
 app.listen(PORT, () => console.log(`Listening...`));
