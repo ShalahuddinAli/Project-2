dc147840e679b654131f4f044bf2251ba22c8f41
shalahuddin-ali <m.shalahuddin.ali@gmail.com>
2021-07-30 05:49:16 +0800

    pull from remote

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('build'));
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-// 	});
-// }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	});
+}
 app.use(cors());
 app.use(express.json());
 
