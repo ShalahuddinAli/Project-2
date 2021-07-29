07c067b827c9bb181e2fd43337675c339d339c5d
shalahuddin-ali <m.shalahuddin.ali@gmail.com>
2021-07-30 05:02:04 +0800

    pull from remote

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	});
+}
 app.use(
 	cors({
 		origin: 'http://localhost:3000',
